d68d76490af482046e0cb732c87a136985995e47
dalisaydavid <dalisay.david3@gmail.com>
2019-09-17 15:35:57 -0400

    Player movement now changes sprite animation.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -15,10 +15,35 @@ func _ready():
 func _input(event):
 	if event is InputEventMouseButton and event.is_pressed():
 		target = get_global_mouse_position()
+		
+func get_direction(angle):	
+	if angle > 1.125 and angle < 1.875:
+		return 'front'
+	elif angle > 1.875 and angle < 2.625:
+		return 'front_left'
+	elif (angle > 2.625 and angle < PI) or (angle > -PI and angle < -2.625):
+		return 'left'
+	elif angle > -2.625 and angle < -1.875:
+		return 'back_left'
+	elif angle > -1.875 and angle < -1.125:
+		return 'back'
+	elif angle > -1.125 and angle < -0.375:
+		return 'back_right'
+	elif angle > -0.375 and angle < 0.375:
+		return 'right'
+	elif angle > 0.375 and angle < 1.125:
+		return 'front_right'
+		
+	return 'default'
 
 func _physics_process(delta):
 	velocity = (target - global_position).normalized() * speed
-#	rotation = velocity.angle()
+		
+	var angle = velocity.angle()
+	var direction = get_direction(angle)
+	if direction != null:
+		$Sprite.animation = direction
+		
 	if (target - global_position).length() > 5:
 		velocity = move_and_slide(velocity)
 
