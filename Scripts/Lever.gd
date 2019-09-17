4aa9c410878343ec8e2eb78b3709a4090d829876
Imgoinghamm <tyhamm10@gmail.com>
2019-09-17 15:47:54 -0700

    added bad particle effects and reverse lever anim

--- Scripts/Lever.gd
+++ Scripts/Lever.gd
@@ -1,13 +1,24 @@
 extends KinematicBody2D
 
 onready var root = get_parent().get_parent()
+onready var state;
 
 func _ready():
+	state = "left"
 	pass
 
 func interact():
-	$AnimationPlayer.play('PullLever')
+	print(state)
+	if state == "left":
+		$AnimationPlayer.play('PullLever');
+		state = "right";
+	elif state == "right":
+		$AnimationPlayer.play('PullLeverReverse');
+		state = "left";
+		
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if anim_name == 'PullLever':
 		root.get_node('Door').open_door()
+	if anim_name == 'PullLeverReverse':
+		root.get_node('Door').close_door()
