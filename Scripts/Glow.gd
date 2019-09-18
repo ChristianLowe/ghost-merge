8c393fffd67087eeff3bf6639ca7e900c5002a8a
dalisaydavid <dalisay.david3@gmail.com>
2019-09-18 17:05:00 -0400

    Added animation for suit of armor.

--- Scripts/Glow.gd
+++ Scripts/Glow.gd
@@ -4,12 +4,17 @@ func _ready():
 	var parent_sprite = get_parent().get_node('Sprite')
 	var sprite = get_node('Sprite')
 
-	sprite.texture = parent_sprite.texture
-	sprite.global_position = parent_sprite.global_position
-	sprite.vframes = parent_sprite.vframes
-	sprite.hframes = parent_sprite.hframes
-	sprite.frame = parent_sprite.frame
+	if parent_sprite.is_class('Sprite'):
+		sprite.texture = parent_sprite.texture
+		sprite.vframes = parent_sprite.vframes
+		sprite.hframes = parent_sprite.hframes
+		sprite.frame = parent_sprite.frame
+	elif parent_sprite.is_class('AnimatedSprite'):
+		var parent_frame = parent_sprite.get_frame()
+		var parent_animation = parent_sprite.get_animation()
+		sprite.texture = parent_sprite.get_sprite_frames().get_frame(parent_animation, parent_frame)
 
+	sprite.global_position = parent_sprite.global_position
 	sprite.centered = true
 	parent_sprite.centered = true
 
