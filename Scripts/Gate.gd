b7e1ebf4b90be2c54e2447defafac7108ee8e65a
dalisaydavid <dalisay.david3@gmail.com>
2019-09-20 10:21:59 -0400

    Added Gate and Plate animations. Added level 6.

--- Scripts/Gate.gd
+++ Scripts/Gate.gd
@@ -1,11 +1,25 @@
 extends Node2D
 
+onready var is_open: bool = false
+
+func _ready():
+	$Sprite2.frame = 0
+
 func open():
+	if is_open:
+		return
+		
 	SoundController.play_effect("res://Sounds/Door.wav", -40)
 	print('the gate... is opened.')
-#	$StaticBody2D/CollisionShape2D.queue_free()
 	$StaticBody2D/CollisionShape2D.set_deferred('disabled', true)
+	$Sprite2/AnimationPlayer.play('open')
+	is_open = true
 	
 func close():
+	if not is_open:
+		return
+		
 	print('the gate... is closed.')
 	$StaticBody2D/CollisionShape2D.set_deferred('disabled', false)
+	$Sprite2/AnimationPlayer.play('close')
+	is_open = false
--- Scripts/PressurePlate.gd
+++ Scripts/PressurePlate.gd
@@ -2,11 +2,28 @@ extends Node2D
 
 export (NodePath) var gate_path
 
+onready var interactable_objects: Array = []
+
 func _on_Area2D_body_entered(body):
 	if body.get_parent().get_name() == 'Interactable':
 		SoundController.play_effect("res://Sounds/PressurePlate.wav", -20)
-		get_node(gate_path).open()
+
+		interactable_objects.append(body)
+
+		if interactable_objects.size() > 0:
+			get_node(gate_path).open()
+			$Sprite.play('down')
 
 func _on_Area2D_body_exited(body):
 	if body.get_parent().get_name() == 'Interactable':
-		get_node(gate_path).close()
+		
+		var body_on_plate = interactable_objects.find(body)
+		if body_on_plate == null:
+			return
+			
+		interactable_objects.remove(body_on_plate)
+		
+		if interactable_objects.size() == 0:
+			get_node(gate_path).close()
+			$Sprite.play('up')
+			
