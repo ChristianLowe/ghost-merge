a27bdea6f9864e082e5a02207093478e04933b77
Tyler Hamm <tyhamm10@gmail.com>
2019-09-20 12:01:24 -0700

    Gate and Door changes!

--- Scripts/Door.gd
+++ Scripts/Door.gd
@@ -1,21 +1,21 @@
 extends Node2D
 
 onready var root = get_parent()
-var open = false
+var open = false 
 
 func _ready():
 	close_door(true)
 
 func open_door():
 	SoundController.play_effect("res://Sounds/Door.wav", -20)
-	open = true
-	$Sprite.modulate = Color.green
+	_flip_door()
 
 func close_door(init=false):
 	if not init:
 		SoundController.play_effect("res://Sounds/Door.wav", -20)
-	open = false
-	$Sprite.modulate = Color.red
+		_flip_door()
+	else:
+		open = false
 
 func _on_Area2D_body_entered(body):
 	if not open:
@@ -23,3 +23,13 @@ func _on_Area2D_body_entered(body):
 		
 	if 'Player' in body.get_parent().get_name():
 		root.change_scene()
+		
+func _flip_door():
+	open = not open
+	$VerticalDoor.visible = not $VerticalDoor.visible
+	$Area2D/CollisionShapeVerticalDoor.visible = not $Area2D/CollisionShapeVerticalDoor.visible
+	
+	$HorizontalDoor.visible = not $HorizontalDoor.visible
+	$Area2D/CollisionShapeHorizontalDoor.visible = not $Area2D/CollisionShapeHorizontalDoor.visible
+
+	
--- Scripts/Gate.gd
+++ Scripts/Gate.gd
@@ -1,18 +1,24 @@
 extends Node2D
 
 onready var is_open: bool = false
+onready var sprite = $HorizontalGate
+export var is_vertical: bool = false
 
 func _ready():
-	$Sprite2.frame = 0
+	if is_vertical:
+		sprite = $VerticleGate
+		$VerticleGate.visible = true
+		$HorizontalGate.visible = false
+	sprite.frame = 0
 
 func open():
 	if is_open:
 		return
-		
+	
 	SoundController.play_effect("res://Sounds/Door.wav", -40)
 	print('the gate... is opened.')
 	$StaticBody2D/CollisionShape2D.set_deferred('disabled', true)
-	$Sprite2/AnimationPlayer.play('open')
+	sprite.get_node("AnimationPlayer").play('open')
 	is_open = true
 	
 func close():
@@ -21,5 +27,5 @@ func close():
 		
 	print('the gate... is closed.')
 	$StaticBody2D/CollisionShape2D.set_deferred('disabled', false)
-	$Sprite2/AnimationPlayer.play('close')
+	sprite.get_node("AnimationPlayer").play('close')
 	is_open = false
